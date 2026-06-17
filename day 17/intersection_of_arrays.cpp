077b624b731f5312d6bf629b812fde805221f523
OptimisticTachyon <manishmehra31.3.07@gmail.com>
2026-06-17 14:31:15 +0530

    Completed day 17 programs.

---
+++ day 17/intersection_of_arrays.cpp
@@ -0,0 +1,46 @@
+// Program to print the intersection of arrays.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2;
+
+    cout << "Enter size of first array: ";
+    cin >> n1;
+    int arr1[n1];
+    cout << "Enter elements of first array:\n";
+    for (int i = 0; i < n1; i++) {
+        cin >> arr1[i];
+    }
+
+    cout << "Enter size of second array: ";
+    cin >> n2;
+    int arr2[n2];
+    cout << "Enter elements of second array:\n";
+    for (int i = 0; i < n2; i++) {
+        cin >> arr2[i];
+    }
+
+    cout << "Intersection of arrays:\n";
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
+            if (arr1[i] == arr2[j]) {
+                bool printed = false;
+                for (int k = 0; k < i; k++) {
+                    if (arr1[i] == arr1[k]) {
+                        printed = true;
+                        break;
+                    }
+                }
+                if (!printed) {
+                    cout << arr1[i] << " ";
+                }
+                break;
+            }
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
