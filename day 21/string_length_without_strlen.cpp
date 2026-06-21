81b8da6ef7d785d0e6fbc5bf7939313440b6706c
OptimisticTachyon <manishmehra31.3.07@gmail.com>
2026-06-21 17:20:00 +0530

    Completed day 21 programs.

---
+++ day 21/string_length_without_strlen.cpp
@@ -0,0 +1,19 @@
+// Program to find the length of a string without using strlen().
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    cout << "Length of the string = " << length << endl;
+
+    return 0;
+}
